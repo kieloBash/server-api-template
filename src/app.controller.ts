dedb3d13f82f4103d05c108e1b43a63c2a2c6893
Kielo Bash Mercado <kielobashmercado@Kielos-Mac-mini.local>
2026-01-14 21:33:16 +0800

    feat: add health-check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
+  healthCheck(): string {
+    return 'Api is online!';
+  }
+
+  @Get("/users")
   getUsers() {
     return this.appService.getUsers();
   }

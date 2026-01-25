bf627c57c032a24bd373943f4f2c85b48381b151
Kielo Bash Mercado <kielobashmercado@Kielos-Mac-mini.local>
2026-01-25 14:06:51 +0800

    feat: add user on login credentials

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -27,6 +27,7 @@ export class AuthService {
         const payload = { sub: user.id, email: user.email, role: user.role };
         return {
             access_token: this.jwtService.sign(payload),
+            user,
         };
     }
 

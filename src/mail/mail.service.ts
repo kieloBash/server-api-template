c818d98379a484902d9650a01a8171fa36f06fa5
Kielo Bash Mercado <kielobashmercado@Kielos-Mac-mini.local>
2026-01-27 18:26:25 +0800

    feat: replaced  nodemailer with sendgrid

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,32 +1,24 @@
 import { HttpException, Injectable, Logger } from '@nestjs/common';
-import * as nodemailer from 'nodemailer';
-
+import sgMail from '@sendgrid/mail';
 @Injectable()
 export class MailService {
-    private transporter: nodemailer.Transporter;
     private logger = new Logger(MailService.name);
 
     constructor() {
-        this.transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.MAIL_GOOGLE_APP_EMAIL,
-                pass: process.env.MAIL_GOOGLE_APP_PASSWORD,
-            },
-        });
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
     }
 
     async sendMail(to: string, subject: string, html: string) {
         const mailOptions = {
-            from: process.env.MAIL_GOOGLE_APP_EMAIL,
             to,
+            from: process.env.SENDGRID_FROM_EMAIL!,
             subject,
             html,
         };
 
         try {
-            const info = await this.transporter.sendMail(mailOptions);
-            this.logger.log('Email sent: ' + info.response);
+            await sgMail.send(mailOptions);
+            this.logger.log('Email sent to: ' + mailOptions.to);
         } catch (error) {
             this.logger.error('Error sending email: ', error);
             throw new HttpException("Failed to send email", 500);
